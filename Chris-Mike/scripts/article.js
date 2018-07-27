865cd7b20fd4a78a0a8840067d65885e36641cf2
Mike K <m.kermes89@gmail.com>
2018-07-26 21:09:03 -0700

    worked on getters and setters, working out bugs

--- Chris-Mike/scripts/article.js
+++ Chris-Mike/scripts/article.js
@@ -6,7 +6,7 @@ let articles = [];
 // This functions is a constructor function, used to build article objects out of raw data strings.  The name is capitalied as a convention to identify constructor functions.  "this" represents the specific instance that is being currently run through the constructor function.  "rawDataObj" would be the data that is used to create the articles, in this case, the variable 'rawData'.
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object that is passed in to complete this constructor function:
+  // TODONE: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
   this.title = title;
   this.catagory = catagory;
@@ -21,11 +21,12 @@ Article.prototype.toHtml = function() {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
   // PUT YOUR RESPONSE HERE
 
-  let $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
-  $('.template').css('display', 'none');
+  let $newArticle = $('article.template').clone().removeClass('template');
+  /* TODONE: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+  // $('.template').css('display', 'none');
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
+
 
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
@@ -34,6 +35,12 @@ Article.prototype.toHtml = function() {
       3. article title,
       4. article body, and
       5. publication date. */
+
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('a').html(this.author);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('time').attr('datetime', this.publishedOn);
 
   // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + Math.floor((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
@@ -56,8 +63,7 @@ for(let i = 0; i < articles.length; i++) {
 
   // REVIEW: below code will hang until TODO about cloned article is handled
   // Once that TODO is done uncomment code
-   
-  // $('#articles').append(articles[i].toHtml());
+  $('#articles').append(articles[i].toHtml());
 
   // COMMENT: (STRETCH) Can you figure out why code hangs?
   // It has to do with the clone() method
